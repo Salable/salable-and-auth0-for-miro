0cf5e1eea05a4691f06c853da84574cdcc6952a1
nealriley <neal.riley@gmail.com>
2022-08-04 16:22:44 +0000

    working salable prototype

---
+++ src/lib/salable/license.js
@@ -0,0 +1,42 @@
+const getLicence = async () => {
+    // const user = await miro.board.getUserInfo();
+    const productUuid = "5108c23c-efe4-40f7-9043-0caed0b3cbd4"
+    const url = `https://api.salable.org/licenses/check?productUuid=${productUuid}&granteeIds=[1234567]`
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': 'qInsLxVYeT6GOPaNq2qAR4rFW1ZrBuVS9uHJIjUH'
+            }
+            })
+        if (response.status === 200) return response.json()
+        return null
+    } catch (err) {
+        console.log(err)
+        return null
+    }
+  }
+
+  export const getLicensesForPurchaser = async (email) => {
+    // const user = await miro.board.getUserInfo();
+    const productUuid = "5108c23c-efe4-40f7-9043-0caed0b3cbd4"
+    console.log(email)
+    const url = `https://api.salable.org/licenses/purchaser?productUuid=${productUuid}&purchaser=${email}`
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': 'qInsLxVYeT6GOPaNq2qAR4rFW1ZrBuVS9uHJIjUH'
+            }
+            })
+        if (response.status === 200) return response.json()
+        return null
+    } catch (err) {
+        console.log(err)
+        return null
+    }
+  }
+  
+  export default getLicence
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react"; 
+import getLicence, {getLicensesForPurchaser} from "../lib/salable/license";
+
 
 const Home = () => {  
   // function useOnceCall(cb, condition = true) {
@@ -13,15 +15,56 @@ const Home = () => {
   //   }, [cb, condition]);
   // } 
 
+  const licenseCheckFunction = async (email) => {
+    let hasLicenses = await getLicence(email)
+    if (hasLicenses) {
+      let result = await getLicensesForPurchaser(email)
+      callSalablePricingTable()
+      console.dir(result)
+    }    
+  }
+
+  // useOnceCall(async () => {
+    
+  // })
+
+  const callSalablePricingTable = async () => {
+    const Salable = window.Salable
+    console.dir(Salable)
+    const salable = new Salable(
+      {
+        pricingTableNode: document.querySelector('#pricing-table'),
+        productUuid: '5108c23c-efe4-40f7-9043-0caed0b3cbd4',
+        apiKey: 'qInsLxVYeT6GOPaNq2qAR4rFW1ZrBuVS9uHJIjUH',
+        globalPlanOptions: {
+          granteeId: 'example-grantee-id-123',
+          successUrl: 'https://example.com/success',
+          cancelUrl: 'https://example.com/cancel',
+          contactUsLink: 'https://example.com/contact'
+        }
+      },
+      {
+        member: 'example-member-123',
+        customer: {
+          email: "person@company.com"
+        }
+      }
+    );
+    await salable.init();
+  }
   // Render the content for the home page
 const RenderHomeContent = (segmentKey) => {
   // Return loading, authenticated states, provide button to start login
-  const { isLoading, isAuthenticated, loginWithPopup} = useAuth0();
+  const { isLoading, isAuthenticated, loginWithPopup, user} = useAuth0();
   if (isLoading) {
     return <></>;
   }
-  if (isAuthenticated) {  
-    return<>Authed!</>
+  if (isAuthenticated) {          
+    console.dir(user)
+    licenseCheckFunction(user.email)    
+    return<>
+    {user.email}
+    </>
   }
   
   return <>
@@ -43,8 +86,11 @@ const RenderHomeContent = (segmentKey) => {
   return (
     <>
       <h1>Salable with Miro auth</h1>      
+      
       <div>
       {RenderHomeContent()}
+      </div>
+      <div id="pricing-table">
       </div>
     </>
   );
